ec7bb7944f19013ed3a2fb15978c9071903524ce
VikTor <offcre.vikt@gmail.com>
2018-09-26 18:15:31 +0300

    pug

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,43 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnext = require('cssnext');
 var precss = require('precss');
+var lost = require('lost');
+var minmax = require('postcss-media-minmax');
+var pxtorem = require('postcss-pxtorem');
+var postcssResponsiveFont = require('postcss-responsive-font');
+
+var pug = require('gulp-pug');
 
 gulp.task('postcss', function () {
     var processors = [
         autoprefixer('last 5 version'),
         cssnext, 
-        precss
+        precss,
+        lost,
+        minmax,
+        pxtorem,
+        postcssResponsiveFont
     ];
     return gulp.src('./src/style/**/*.css')
         .pipe(postcss(processors))
         .pipe(gulp.dest('./dest/css'));
 });
 
+gulp.task('pug', function () {
+    return gulp.src('./src/pug/**/*.pug')
+        .pipe(pug({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./dest/'));
+});
+
 
 // Rerun the task when a file changes
 gulp.task('watch', function () {
     gulp.watch('src/style/style.css', ['postcss']);
+    gulp.watch('src/pug/**/*.pug', ['pug']);
     // gulp.watch(paths.images, ['images']);
 });
 
 // The default task (called when you run `gulp` from cli)
-gulp.task('default', ['watch', 'postcss']);
+gulp.task('default', ['watch', 'postcss', 'pug']);
